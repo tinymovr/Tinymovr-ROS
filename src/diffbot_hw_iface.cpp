7bb0a1be72cc6eb328b5ab011881a97325e97ad3
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-04-15 17:08:34 +0300

    remove scpp deps

--- src/diffbot_hw_iface.cpp
+++ src/diffbot_hw_iface.cpp
@@ -1,24 +1,27 @@
 
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
 #include <tinymovr_ros/diffbot_hw_iface.hpp>
 
 using namespace tinymovr_ros;
-using namespace scpp;
 
 #define CMD_GET_ENC_ESTIMATES 0x1A
 #define CMD_SET_VEL 0x1B
 
 bool Diffbot::read(const ros::Duration& dt)
 {
-    scpp::CanFrame fr;
-
     for (uint i = 0; i < hw_commands_.size(); i++)
     {
-        if (write_frame(make_arbitration_id(hw_node_ids_[i], CMD_GET_ENC_ESTIMATES), 0, 0) != scpp::STATUS_OK)
+        if (write_frame(make_arbitration_id(hw_node_ids_[i], CMD_GET_ENC_ESTIMATES), 0, 0))
         {
             return false;
         }
 
-        if (socket_can.read(fr) != scpp::STATUS_OK)
+        if ()
         {
             return false;
         }
@@ -57,17 +60,7 @@ bool Diffbot::write()
 
 bool Diffbot::write_frame(uint32_t arb_id, const uint8_t *data, uint8_t data_len)
 {
-    scpp::CanFrame cf_to_write;
-        
-    cf_to_write.id = arb_id;
-    cf_to_write.len = data_len;
-    for (int i = 0; i < data_len; ++i)
-        cf_to_write.data[i] = data[i];
-    auto write_sc_status = socket_can.write(cf_to_write);
-    if (write_sc_status != scpp::STATUS_OK)
-        return false;
-    else
-        return true;
+    
 }
 
 uint32_t Diffbot::make_arbitration_id(uint32_t node_id, uint32_t command_id)
