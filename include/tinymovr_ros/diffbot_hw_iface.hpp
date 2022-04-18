3a7ba52c5f36794ad277a7fe6b1fc5991fcc53ab
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-04-18 12:31:09 +0300

    update iface

--- include/tinymovr_ros/diffbot_hw_iface.hpp
+++ include/tinymovr_ros/diffbot_hw_iface.hpp
@@ -14,37 +14,8 @@ namespace tinymovr_ros
 class Diffbot : public hardware_interface::RobotHW
 {
 public:
-    Diffbot() 
-    { 
-        auto status = socket_can.open("can0");
-        if (scpp::STATUS_OK == status)
-        {
-            ROS_INFO("Socketcan opened successfully");
-        }
-        else
-        {
-            ROS_ERROR("Cant' open Socketcan: %d", status);
-            exit(1);
-        }
+    Diffbot();
 
-        // connect and register the joint state interface
-        hardware_interface::JointStateHandle state_handle_a("A", &hw_positions_[0], &hw_velocities_[0], &hw_efforts_[0]);
-        jnt_state_interface.registerHandle(state_handle_a);
-
-        hardware_interface::JointStateHandle state_handle_b("B", &hw_positions_[1], &hw_velocities_[1], &hw_efforts_[1]);
-        jnt_state_interface.registerHandle(state_handle_b);
-
-        registerInterface(&jnt_state_interface);
-
-        // connect and register the joint velocity interface
-        hardware_interface::JointHandle vel_handle_a(jnt_state_interface.getHandle("A"), &hw_commands_[0]);
-        jnt_vel_interface.registerHandle(vel_handle_a);
-
-        hardware_interface::JointHandle vel_handle_b(jnt_state_interface.getHandle("B"), &hw_commands_[1]);
-        jnt_vel_interface.registerHandle(vel_handle_b);
-
-        registerInterface(&jnt_vel_interface);
-    }
     bool read(const ros::Duration& period);
     bool write();
 
@@ -56,6 +27,7 @@ private:
     const double radius = 0.075;  // radius of the wheels
     const double dist_w = 0.38;   // distance between the wheels
     
+    scpp::SocketCan socket_can;
     hardware_interface::JointStateInterface jnt_state_interface;
     hardware_interface::VelocityJointInterface jnt_vel_interface;
     std::vector<double> hw_commands_ {0.0, 0.0};
--- src/diffbot_hw_iface.cpp
+++ src/diffbot_hw_iface.cpp
@@ -12,16 +12,50 @@ using namespace tinymovr_ros;
 #define CMD_GET_ENC_ESTIMATES 0x1A
 #define CMD_SET_VEL 0x1B
 
+Diffbot::Diffbot() 
+{ 
+    auto status = socket_can.open("can0");
+    if (scpp::STATUS_OK == status)
+    {
+        ROS_INFO("Socketcan opened successfully");
+    }
+    else
+    {
+        ROS_ERROR("Cant' open Socketcan: %d", status);
+        exit(1);
+    }
+
+    // connect and register the joint state interface
+    hardware_interface::JointStateHandle state_handle_a("A", &hw_positions_[0], &hw_velocities_[0], &hw_efforts_[0]);
+    jnt_state_interface.registerHandle(state_handle_a);
+
+    hardware_interface::JointStateHandle state_handle_b("B", &hw_positions_[1], &hw_velocities_[1], &hw_efforts_[1]);
+    jnt_state_interface.registerHandle(state_handle_b);
+
+    registerInterface(&jnt_state_interface);
+
+    // connect and register the joint velocity interface
+    hardware_interface::JointHandle vel_handle_a(jnt_state_interface.getHandle("A"), &hw_commands_[0]);
+    jnt_vel_interface.registerHandle(vel_handle_a);
+
+    hardware_interface::JointHandle vel_handle_b(jnt_state_interface.getHandle("B"), &hw_commands_[1]);
+    jnt_vel_interface.registerHandle(vel_handle_b);
+
+    registerInterface(&jnt_vel_interface);
+}
+
 bool Diffbot::read(const ros::Duration& dt)
 {
+    scpp::CanFrame fr;
+
     for (uint i = 0; i < hw_commands_.size(); i++)
     {
-        if (write_frame(make_arbitration_id(hw_node_ids_[i], CMD_GET_ENC_ESTIMATES), 0, 0))
+        if (scpp::STATUS_OK != write_frame(make_arbitration_id(hw_node_ids_[i], CMD_GET_ENC_ESTIMATES), 0, 0))
         {
             return false;
         }
 
-        if ()
+        if (scpp::STATUS_OK != socket_can.read(fr))
         {
             return false;
         }
@@ -29,8 +63,8 @@ bool Diffbot::read(const ros::Duration& dt)
         const float* hw_pos_float = reinterpret_cast<float*>(fr.data);
         const float* hw_vel_float = reinterpret_cast<float*>(fr.data+4);
 
-        hw_positions_[i] = (double)*hw_pos_float;
-        hw_velocities_[i] = (double)*hw_vel_float;
+        hw_positions_[i] = (double)(*hw_pos_float);
+        hw_velocities_[i] = (double)(*hw_vel_float);
     }
     // Update the free-flyer, i.e. the base notation using the classical
     // wheel differentiable kinematics
@@ -60,7 +94,17 @@ bool Diffbot::write()
 
 bool Diffbot::write_frame(uint32_t arb_id, const uint8_t *data, uint8_t data_len)
 {
-    
+    scpp::CanFrame cf_to_write;
+
+    cf_to_write.id = arb_id;
+    cf_to_write.len = data_len;
+    for (int i = 0; i < data_len; ++i)
+        cf_to_write.data[i] = data[i];
+    auto write_sc_status = socket_can.write(cf_to_write);
+    if (write_sc_status != scpp::STATUS_OK)
+        return false;
+    else
+        return true;
 }
 
 uint32_t Diffbot::make_arbitration_id(uint32_t node_id, uint32_t command_id)
